fdb66146250dcebab1e3507d9de145d58c11445a
Lars Føien <larsmustars@gmail.com>
2021-10-01 11:59:21 +0200

    edit bet

--- Oblig 1.py
+++ Oblig 1.py
@@ -3,25 +3,44 @@ def bet():
 	bet = 0
 	bet = input("Enter a how much you want to bet?: ")
 	if bet.isdigit():
-		bet = int(bet)
 		if bet >= 1:
+			bet = int(bet)
 			print(f"{bet} is an accepted amount")
 		else :
 			print(f"{bet} is not an accepted amount")
 			main()			
 	else:
-		print("You must input the amount you want to bet")
+		print(f"'{bet}' is an invalid input")
 		main()
-		
+
+def guess():
+	guess = input("What is your guess? (1-50): ")
+	if guess.isdigit():
+		guess = int(guess)
+		if guess >= 1 and guess <= 50:
+			print(f"you entered {guess}")
+		else :
+			print(f"{guess} is not an accepted guess")
+			main()			
+	else:
+		print(f"'{guess}' is an invalid guess")
+		main()
+
+
+				
+				
 #def game1():
 #	pass		
 #		
 #
-def main():
+def start():
 	bet()
+
+def main():
+	guess()
 #	
 #	while bet > 0:
 #		pass
 #	
 	
-main()
+guess()
