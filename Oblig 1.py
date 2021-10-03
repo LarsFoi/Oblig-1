8b16277f2c64550abcfe08d79e4b02e086da5329
LarFoi <larsmustars@gmail.com>
2021-10-03 12:02:58 +0200

    defined number

--- Oblig 1.py
+++ Oblig 1.py
@@ -1,23 +1,22 @@
 ### script header ###
 import sys
-
-
+bet_num = 0
+guess_num = 0
 
 
 ### Enter bet-def ### -lars
 def bet():
-	bet_num = 0
 	bet_num = input("Enter a how much you want to bet?: ")
 	if bet_num.isdigit():
 		bet_num = int(bet_num)
 		if bet_num >= 1:
-			print(f"{bet} is an accepted amount")
-		else :
-			print(f"{bet} is not an accepted amount")
+			print(f"{bet_num} is an accepted amount")
+			return int(bet_num)
+		else:
+			print(f"{bet_num} is not an accepted amount")
 			main()
-		return bet_num			
 	else:
-		print(f"'{bet}' is an invalid input")
+		print(f"'{bet_num}' is an invalid input")
 		main()
 
 ### Enter guess-def ### -lars
@@ -26,12 +25,12 @@ def guess():
 	if guess_num.isdigit():
 		guess_num = int(guess_num)
 		if guess_num >= 1 and guess_num <= 50:
-			print(f"you entered {guess}")
+			print(f"you entered {guess_num}")
 		else :
-			print(f"{guess} is not an accepted guess")
+			print(f"{guess_num} is not an accepted guess")
 			main()			
 	else:
-		print(f"'{guess}' is an invalid guess")
+		print(f"'{guess_num}' is an invalid guess")
 		main()
 		
 ### start sequence ### -lars	
@@ -55,7 +54,7 @@ while bet_num > 1:
 		# medforsøk 1,2,3 #
 	
 	
-while bet_num <= int(0):
+while bet_num <= 0:
 	print("play again?")
 	x = input("'y'es or 'n'o")
 	if x == "y":
