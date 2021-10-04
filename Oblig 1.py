f1c6424c833413f3d33965cc18618012a8b9d3c7
Lars Føien <larsmustars@gmail.com>
2021-10-04 17:08:49 +0200

    edit

--- Oblig 1.py
+++ Oblig 1.py
@@ -1,11 +1,23 @@
+########---to do---##############################
+# 1. legg til balance i start sequence
+# 2. 
+# 3. 
+# 4. 
+#################################################
+
 ### script header ###
 import sys
 from random import randrange
 bet_num = 0
 guess_num = 0
 random_num = 0
+balance = 0
 
-### Enter bet-def ### -lars
+### start sequence --- ###
+def start():
+	bet()
+
+### Enter bet-def ###
 def bet():
 	global bet_num
 	bet_num = input("Enter a how much you want to bet?: ")
@@ -33,36 +45,38 @@ def guess():
 		print(f"'{guess_num}' is an invalid guess")
 	return int(guess_num)
 
-### start sequence ###
-def start():
-	bet()
-
 ### main ###
 def main():
-	guess()
 	global bet_num
 	global guess_num
+	
+	## guess 1 ##
+	guess()
 	random_num = int(randrange(1,50))
-	print(f"the number is {random_num}")
+	print(f"###the number is {random_num}###") ### debugging - bare sånn at vi kan se hva tallet er ###
 	if int(guess_num) == int(random_num):
 		bet_num = bet_num * 10
 		print(f"your score is {bet_num}")
 		return int(bet_num)
 	else:
-		if bet_num > random_num:
+		if guess_num > random_num:
 			print("too hight, you have 2 etempts left")
 		else:
 			print("to low; you have 2 etempts left")
+	
+	## guess 2 ##
 	guess()
 	if guess_num == random_num:
 		bet_num = bet_num * 5
 		print(f"your score is {bet_num}")
 		return int(bet_num)
 	else:
-		if bet_num > random_num:
+		if guess_num > random_num:
 			print("too hight, you have 2 etempts left")
 		else:
 			print("to low; you have 2 etempts left")
+			
+	## guess 3 ##
 	guess()
 	if guess_num == random_num:
 		bet_num = bet_num * 3
@@ -78,7 +92,7 @@ def main():
 start()
 main()
 
-### main game loop ###
+### game loop ###
 while bet_num > 0:	
 	print(f"You won! play again? Your score is {bet_num}")
 	x = input("'y'es or 'n'o: ")
@@ -88,7 +102,7 @@ while bet_num > 0:
 		sys.exit()
 	
 while bet_num <= 0:
-	print("play again?")
+	print("You lose, play again?")
 	x = input("'y'es or 'n'o: ")
 	if x == "y":
 		start()
