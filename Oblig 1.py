09c0d4e5d1aa7e6f6eefc4c6164b2f54ecb448da
Håkon Mindrebøe <haakon_686@hotmail.com>
2021-10-05 23:05:25 +0200

    Ny update

--- Oblig 1.py
+++ Oblig 1.py
@@ -4,14 +4,13 @@ Created on Mon Oct  4 17:26:07 2021
 
 @author: håkon
 """
-
-# Har her satt at man kan kalle seg hva som helst, .,-po9 hvis ønskelig..
-# Har bare en funskjon, er jo egentlig unødvendig det. 
+# Har her satt at man kan kalle seg hva som helst, .,-po9 hvis ønskelig.. 
 # ellers tror jeg den fungerer som den skal
-
+# la nå til at initial amount må være større enn 0, men en liten bug der..
+# syntes det er litt vanskelig å gjengi hva og hvorfor jeg har gjort som jeg har gjort de ulike stedene, litt vanskelig og rotete men..
 
 from random import randint
-
+# start meny
 print('%3s' % '-','%0s' % '', 'Choose any number between 1 and 50.')
 print('%3s' % '-','%0s' % '', 'If you guess the number correctly at your 1.st attempt, you will get 10 times the money you bet')
 print('%3s' % '-','%0s' % '', 'If you guess the number correctly at your 2.nd attempt, you will get 5 times the money you bet')
@@ -20,19 +19,42 @@ print('%3s' % '-','%0s' % '', 'If you do not guess the number correctly within t
 
 name = input('Please enter your name: ')
 
-initial_amount = input('Enter your initial amount: ')
-if initial_amount.isdigit() is False:
-    condition = True
-    while condition:
-        print('Invalid input')
-        initial_amount = input('Enter your initial amount: ')
-        if initial_amount.isdigit() is True:
-            condition = False
+# kjører mye while utifra om condition er true eller false, gjør at ting repiteres helt til input er gyldig.
 
-    
+initial_amount = input('Enter your initial amount: ')
+
+# ------ loop for initial amount ---------#
+condition = True
+while condition:
+    if initial_amount.isdigit() is False:               
+        condition = True
+        while condition:
+            print('Invalid input')
+            initial_amount = input('Enter your initial amount: ')
+            if initial_amount.isdigit() is True:
+                condition = False
+            else:
+                print('',end='')
+                
+    elif int(initial_amount) <= 0:
+            condition = True
+            print('Invalid input')
+            while condition:
+                initial_amount = input('Enter your initial amount: ')
+                if initial_amount.isdigit() is True:
+                    if int(initial_amount) <= 0:
+                        print('Invalid input')
+                    else:
+                        condition = False
+                else:
+                    print('Invalid input')
+    else:
+        condition = False
+#---------- loop slutt ------------------#            
 
 def function():
     global initial_amount
+    # ---------- loop betting amount -------------#
     condition = True
     while condition:
         betting_amount = input('Enter a betting amount: ')
@@ -43,8 +65,6 @@ def function():
                 betting_amount = input('Enter your betting amount: ')
                 if betting_amount.isdigit() is True:
                     condition = False
-                else:
-                    print('',end='')
         else:
             condition = False
             
@@ -63,10 +83,9 @@ def function():
                             betting_amount = input('Enter your betting amount: ')
                             if betting_amount.isdigit() is True:
                                 condition = False
-                            else:
-                                print('',end='')
-                    else:
-                        print('',end='')
+            elif int(betting_amount) == 0:
+                print('You got to bet something!')
+                break
             else:
                 print('You can not bet a negative number')
                 betting_amount = input('Enter a betting amount: ')
@@ -77,12 +96,10 @@ def function():
                         betting_amount = input('Enter your betting amount: ')
                         if betting_amount.isdigit() is True:
                             condition = False
-                        else:
-                            print('',end='')
-                else:
-                    print('',end='')
-                
-    i = 3            
+     #--------- loop slutt --------------------#
+    
+    i = 3 
+    #------- loop for num guess (bare første gang) --------#
     condition = True
     while condition:
         num_guess = input('Enter a number between 1 and 50: ')
@@ -101,10 +118,13 @@ def function():
                     print(f'Invalid input, you got {i} attempts left')
             else:
                 print(f'Invalid input, you got {i} attempts left')
+    #---------- loop slutt -----------#
     
     i = 0
     random = randint(1,50)
     winning_amount = 0
+    
+    #---------- loop for eventuell gevinst og de to neste num guess ------#
     while i < 2:
         if i == 0 and random == int(num_guess):
             winning_amount = int(betting_amount) * 10
@@ -122,7 +142,7 @@ def function():
             if int(num_guess) < random:
                 print(f'Too low, you got {2-i} attempts left')
             else:
-                print(f'Too high, you got {2-i} attempts left')
+                print(f'Too high, you got {2-i} attempts left')         # her er (i) = 2-i, slik at (i) går fra 2 til 1 i loopen. (har i=i+1 i slutten av loopen)
                 
         condition = True
         while condition:
@@ -143,39 +163,43 @@ def function():
                 print(f'Invalid input, you got {2-i} attempts left')
         
         i = i + 1
+        #------------ loop slutt -----------#
         
     if winning_amount == 0:
         print('You lost your betting amount')
     else:
         print(f'Congratulationt {name}! You won:', winning_amount, '$')
     
+    # Utregningen på hvor mye som er igjen på spill-kontoen:
+        
     initial_amount = int(initial_amount) - int(betting_amount) + winning_amount
     
     if initial_amount == 0:
         print()
         print(f'Sorry {name}, you lost all your money, thanks for playing!')
-        
-    
-    
     else:
         print('Your initial amount is:', int(initial_amount), '$')
         print('Play again?',end='')
         game = input("'y'es or 'n'o: ")
         if game == 'y':
-            function()
+            function()      # kjører hele funksjonen på nytt hvis 'y'
         elif game == 'n':
             print()
             print(f'Thanks for the game, {name}! You got {initial_amount} $')
         else:
-            while game != 'y' or game != 'n':
+            condition = True
+            while condition:
                 print('Invalid input')
                 game = input("'y'es or 'n'o: ")
                 if game == 'y':
+                    condition = False
                     function()
                 elif game == 'n':
                     print()
                     print(f'Thanks for the game, {name}! You got {initial_amount} $')
                     break
+                else:
+                    print('',end='')
         
             
 function()
