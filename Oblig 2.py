b8d6791e83b226ce2e53c69ad65b5e0e9dba54d8
Håkon Mindrebøe <haakon_686@hotmail.com>
2021-11-09 11:57:28 +0100

    Update Oblig 2.py

--- Oblig 2.py
+++ Oblig 2.py
@@ -15,11 +15,12 @@ def login_info():
 if login_info():
     print('Valid input!')
 
+### en måte å tenke på...
 def q1():
     input_1 = input('What is the capital of Norway?: ')
     if input_1 == 'oslo':
-        return 1
-print(q1())
+        return 1   ## returner 1 hvis svaret er riktig... kan dermed holde styr på hvilket svar som var riktig, hvis vi har en funksjon for hvert spørsmål/svar...
+print(q1())  ## blir 1 hvis input_1 = oslo..
 
 
 score = 0
