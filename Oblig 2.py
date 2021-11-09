2c63364a51387acd5777d908759f21e9d8b076bc
Lars Føien <larsmustars@gmail.com>
2021-11-09 12:30:51 +0100

    edit

--- Oblig 2.py
+++ Oblig 2.py
@@ -1,52 +1,68 @@
 #Gruppe 30 - Håvard, Håkon, Lars, Michael
+i = 0
 score = 0
-i = 1
-
 FA = []
 FQ = []
 
-Q = ["What is the capital of Norway?":2,"What is the currency of Norway?":3,"What is the largest city in Norway?":1,"When is constitution day (the national day) of Norway?":2,"What color is the background of the Norwegian flag?":1,"How many countries does Norway border?":3,"What is the name of the university in Trondheim?":4,"How long is the border between Norway and Russia?":2,"Where in Norway is Stavanger?":3,"From which Norwegian city did the world’s famous composer Edvard Grieg come?":2]
+Q = ["What is the capital of Norway?",
+"What is the currency of Norway?",
+"What is the largest city in Norway?",
+"When is constitution day (the national day) of Norway?",
+"What color is the background of the Norwegian flag?",
+"How many countries does Norway border?",
+"What is the name of the university in Trondheim?",
+"How long is the border between Norway and Russia?",
+"Where in Norway is Stavanger?",
+"From which Norwegian city did the world’s famous composer Edvard Grieg come?"]
 
-A1 = [
-			["","","",""]
-			["","","",""]
-			["","","",""]
-			["","","",""]
-			["","","",""]
-			["","","",""]
-			["","","",""]
-			["","","",""]
-			["","","",""]
+CA = [2,3,1,2,1,3,4,2,3,2]
+
+A = [
+			["","","",""],
+			["","","",""],
+			["","","",""],
+			["","","",""],
+			["","","",""],
+			["","","",""],
+			["","","",""],
+			["","","",""],
+			["","","",""],
 			["","","",""]
 ]
 								
 def main(): 
-
-for i in Q:
-	condition = True
-	while condition:
-		answer = input(f'What is the correct answer (1-4?) to question {i}: ')
-		if answer.isdigit() is False:
-			print('Invalid input')
-		elif answer[0] == '0':
-			print('Invalid input')
-		elif int(answer) > 0:
-			if int(answer) <= 4:
-				condition = False
-			else:
-				print(f'Invalid input')
-	else:
-		print(f'Invalid input')
+	global score
+	for i in range(10):	
+		print(Q[i])
+		print(A[i])
 	
-	i = i + 1 
+		condition = True
+		while condition:
+			answer = input(f'What is the correct answer (1-4?) to question {i+1}: ')
+			if answer.isdigit() is False:
+				print('Invalid input1')
+			elif answer[0] == '0':
+				print('Invalid input2')
+			elif int(answer) > 0:
+				if int(answer) <= 4:
+					condition = False 
+				else:
+					print(f'Invalid input3')
+			else:
+				print(f'Invalid input4')
 		
-	if answer == Q.keys(i): ### list[i]
-		score = score + 1
-		print("next question")
-	else:
-		print("next question")
-		FQ.append(i)
-		FA.append(answer)
+		if int(answer) == CA[i]: ### list[i]
+			score = score + 1
+			print("'C'next question")
+			
+			
+			i = i + 1
+		else:
+			print("'w'next question")
+			FQ.append(i)
+			FA.append(answer)
+			
+			i = i + 1
 		
 			
 
