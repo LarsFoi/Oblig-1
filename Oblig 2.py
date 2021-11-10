149df547407cf207fe1f6150120d2d5684d2e895
Håkon Mindrebøe <haakon_686@hotmail.com>
2021-11-10 08:13:20 +0100

    Update Oblig 2.py

--- Oblig 2.py
+++ Oblig 2.py
@@ -1,33 +1,3 @@
-1111111111
-
-score = 0
-i = 0
-
-login_keys = {'username': 'MEK1300', 'password': 'Python'}
-
-def login_info():
-    condition = True
-    while condition:
-        username1 = input('Enter the username: ')
-        password1 = input('Enter the password: ')
-        if username1 == login_keys['username'] and password1 == login_keys['password']:
-            return True
-        else:
-            print('Invalid username and/or password')
-
-if login_info():
-    print('Valid input!')
-
-### en måte å tenke på -->
-#def q1():
-#    input_1 = input('What is the capital of Norway?: ')
-#    if input_1 == 'oslo':
-#        return 1   ## returner 1 hvis svaret er riktig... kan dermed holde styr på hvilket svar som var riktig, hvis vi har en funksjon for hvert spørsmål/svar...
-#print(q1())  ## blir 1 hvis input_1 = oslo..
-
-FA = []
-FQ = []
-
 Q = ["What is the capital of Norway?",
 "What is the currency of Norway?",
 "What is the largest city in Norway?",
@@ -39,57 +9,107 @@ Q = ["What is the capital of Norway?",
 "Where in Norway is Stavanger?",
 "From which Norwegian city did the world’s famous composer Edvard Grieg come?"]
 
-CA = [2,3,1,2,1,3,4,2,3,2]
-
-A = [
-			["","","",""],
-			["","","",""],
-			["","","",""],
-			["","","",""],
-			["","","",""],
-			["","","",""],
-			["","","",""],
-			["","","",""],
-			["","","",""],
-			["","","",""]
-]
-								
-def main(): 
-	global score
-	for i in range(10):	
-		print(Q[i])
-		print(A[i])
-	
-		condition = True
-		while condition:
-			answer = input(f'What is the correct answer (1-4?) to question {i+1}: ')
-			if answer.isdigit() is False:
-				print('Invalid input1')
-			elif answer[0] == '0':
-				print('Invalid input2')
-			elif int(answer) > 0:
-				if int(answer) <= 4:
-					condition = False 
-				else:
-					print(f'Invalid input3')
-			else:
-				print(f'Invalid input4')
-		
-		if int(answer) == CA[i]: ### list[i]
-			score = score + 1
-			print("'C'next question")
-			
-			
-			i = i + 1
-		else:
-			print("'w'next question")
-			FQ.append(i)
-			FA.append(answer)
-			
-			i = i + 1
-		
-			
+A1 = ["a. Bergen", "b. Oslo", "c. Stavanger", "d. Trondheim"]
+A2 = ["a. Euro", 'b. Pound', 'c. Krone', 'd. Deutsche Mark']
+A3 = ['a. Oslo', 'b. Stavanger', 'c. Bergen', 'd. Trondheim']
 
 
+def qna1():
+    FA = []
+    condition = True
+    while condition:
+        print(Q[0])
+        print(A1[0])
+        print(A1[1])
+        print(A1[2])
+        print(A1[3])
+        user_input = input('a, b, c or d: ').lower()
+        if user_input not in ('abcd').lower():
+            print('Invalid input')
+        elif user_input == ('b').lower():
+            return 1
+        else:
+            if user_input == ('a').lower():
+                user_input = 'a. Bergen'
+            elif user_input == ('c').lower():
+                user_input = 'c. Stavanger'
+            elif user_input == ('d').lower():
+                user_input = 'd. Trondheim'
+            FA.append(Q[0])
+            FA.append(user_input)
+            FA.append(A1[1])
+            FA = 'Wrong answer to:',FA[0],'You answered',FA[1],'The right answer is:',FA[2]
+            return FA      
 
-main()
+def qna2():
+    FA = []
+    condition = True
+    while condition:
+        print(Q[1])
+        print(A2[0])
+        print(A2[1])
+        print(A2[2])
+        print(A2[3])
+        user_input = input('a, b, c or d: ').lower()
+        if user_input not in ('abcd').lower():
+            print('Invalid input')
+        elif user_input == ('c').lower():
+            return 1
+        else:
+            if user_input == ('a').lower():
+                user_input = 'a. Euro'
+            elif user_input == ('b').lower():
+                user_input = 'b. Pound'
+            elif user_input == ('d').lower():
+                user_input = 'd. Deutsche Mark'
+            FA.append(Q[1])
+            FA.append(user_input)
+            FA.append(A2[2])
+            FA = 'Wrong answer to:',FA[0],'You answered',FA[1],'The right answer is:',FA[2]
+            return FA
+
+def qna3():
+    FA = []
+    condition = True
+    while condition:
+        print(Q[2])
+        print(A3[0])
+        print(A3[1])
+        print(A3[2])
+        print(A3[3])
+        user_input = input('a, b, c or d: ').lower()
+        if user_input not in ('abcd').lower():
+            print('Invalid input')
+        elif user_input == ('a').lower():
+            return 1
+        else:
+            if user_input == ('b').lower():
+                user_input = 'b. Stavanger'
+            elif user_input == ('c').lower():
+                user_input = 'c. Bergen'
+            elif user_input == ('d').lower():
+                user_input = 'd. Trondheim'
+            FA.append(Q[2])
+            FA.append(user_input)
+            FA.append(A3[0])
+            FA = 'Wrong answer to:',FA[0],'You answered',FA[1],'The right answer is:',FA[2]
+            return FA  
+        
+qna1 = qna1()
+print()
+qna2 = qna2()
+print()
+qna3 = qna3()
+print()
+
+qna = [qna1, qna2, qna3]
+
+count = 0
+for element in qna:
+    if element == 1:
+        count = count + 1
+print(f'You got {count} right question(s)!')
+
+for element in qna:
+    if element != 1:
+        print (element)
